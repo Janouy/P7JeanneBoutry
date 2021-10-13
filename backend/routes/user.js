94a73f8afc1d9aea999544b8a40472e3cfbbd55f
Janouy <jeanne.boutry@hotmail.fr>
2021-10-13 10:41:53 +0200

    mise en place backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 const auth = require('../middleware/auth');
+const multer = require ('../middleware/multer-config');
 
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
-router.put("/:id", userCtrl.modifyUser);
+router.put("/:id", auth, multer, userCtrl.modifyUser);
 router.get("/:id", userCtrl.getOneUser);
 router.get("/", userCtrl.getAllUsers);
-router.delete("/:id", userCtrl.deleteUser);
+router.delete("/:id", auth, userCtrl.deleteUser);
 
 module.exports = router;
